7b32cac2fd8315464dfd1ad7894ee4b0dc382a60
davide <davide.baiocchi97@gmail.com>
2021-09-16 15:16:23 +0200

    ponte

--- project/database/seeds/DatabaseSeeder.php
+++ project/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PostsTableSeeder::class,
-            CategoriesTableSeeder::class
+            CategoriesTableSeeder::class,
+            TagSeeder::class
         ]);
     }
 }
